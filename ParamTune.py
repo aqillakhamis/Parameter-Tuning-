f554d8a53df50d184cd4c16c442153b04b76d14d
Nik Mohamad Aizuddin b. Nik Azmi <nik-mohamad-aizuddin@yandex.com>
2019-11-13 05:18:11 +0800

    Remove trailing whitespaces

--- ParamTune.py
+++ ParamTune.py
@@ -32,64 +32,46 @@ def create_model(optimizer='adam'):
 	# Compile model
 	model.compile(loss='mean_squared_error', optimizer=optimizer)
 	return model
-   
+
 for train, test in kf.split(x_data): 
     fold+=1
-    
+
     x_train = x_data[train]
     y_train = y_data[train]
-    
+
     x_test = x_data[test]
     y_test = y_data[test]
-    
+
     print("Fold #{}: train={}, test={}".format(fold,train,test)) 
-          
+
     #data standardization 
     std_scale_x = preprocessing.StandardScaler().fit(x_train)
     std_scale_y = preprocessing.StandardScaler().fit(y_train)
-    
+
     x_scale_train = std_scale_x.transform(x_train)
     x_scale_test = std_scale_x.transform(x_test)
-    
+
     y_scale_train = std_scale_y.transform(y_train)
     y_scale_test = std_scale_y.transform(y_test)
-    
+
     #data normalization
     normalized = MinMaxScaler()
     normalized.fit(x_scale_train)
-    
+
     xtrain = normalized.transform(x_scale_train)
     xtest = normalized.transform(x_scale_test)
-    
+
     norm = MinMaxScaler()
     norm.fit(y_scale_train)
-    
+
     ytrain = norm.transform(y_scale_train)
     ytest = norm.transform(y_scale_test)
-       
+
     validator = KerasRegressor(build_fn=create_model, epochs=1000, verbose=0)
-    
+
     #define the grid search parameter
     optimizer = ['Adagrad','Adam']
     param_grid = dict(optimizer=optimizer)
-    
+
     NNmodel = GridSearchCV(estimator=validator, param_grid=param_grid, scoring='neg_mean_squared_error', n_jobs=-1, cv=None)
     NNmodel.fit(xtrain, ytrain, batch_size=None)
-    
-    
-
-
-    
-    
-
-    
-
-    
-    
-        
-
-    
-    
-
-    
-        
