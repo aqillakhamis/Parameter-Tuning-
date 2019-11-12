ce4fd72314ba30d67bf7b916b529c9999db8a6b6
Nurulaqilla Khamis <aqillakhamis@gmail.com>
2019-11-12 16:36:31 +0800

    Add files via upload

---
+++ ParamTune.py
@@ -0,0 +1,95 @@
+#test hyperparameter tuning 
+
+#import modules 
+import numpy as np 
+import pandas as pd 
+import time
+import tensorflow.keras
+import xlsx_utils as xlsx 
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.model_selection import train_test_split, KFold, GridSearchCV
+from sklearn import preprocessing 
+from pandas import ExcelWriter, ExcelFile 
+from keras.wrappers.scikit_learn import KerasRegressor
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras import optimizers, initializers 
+
+#read dataset 
+x_data, y_data, header = xlsx.read('6sample.xlsx')
+
+#kFold
+kf = KFold(3, shuffle=True)
+fold = 0
+
+#create NN model 
+def create_model(optimizer='adam'):
+    # create model
+	model = Sequential()
+	model.add(Dense(10, input_dim=3, activation='tanh'))
+	model.add(Dense(2, activation='linear'))
+	# Compile model
+	model.compile(loss='mean_squared_error', optimizer=optimizer)
+	return model
+   
+for train, test in kf.split(x_data): 
+    fold+=1
+    
+    x_train = x_data[train]
+    y_train = y_data[train]
+    
+    x_test = x_data[test]
+    y_test = y_data[test]
+    
+    print("Fold #{}: train={}, test={}".format(fold,train,test)) 
+          
+    #data standardization 
+    std_scale_x = preprocessing.StandardScaler().fit(x_train)
+    std_scale_y = preprocessing.StandardScaler().fit(y_train)
+    
+    x_scale_train = std_scale_x.transform(x_train)
+    x_scale_test = std_scale_x.transform(x_test)
+    
+    y_scale_train = std_scale_y.transform(y_train)
+    y_scale_test = std_scale_y.transform(y_test)
+    
+    #data normalization
+    normalized = MinMaxScaler()
+    normalized.fit(x_scale_train)
+    
+    xtrain = normalized.transform(x_scale_train)
+    xtest = normalized.transform(x_scale_test)
+    
+    norm = MinMaxScaler()
+    norm.fit(y_scale_train)
+    
+    ytrain = norm.transform(y_scale_train)
+    ytest = norm.transform(y_scale_test)
+       
+    validator = KerasRegressor(build_fn=create_model, epochs=1000, verbose=0)
+    
+    #define the grid search parameter
+    optimizer = ['Adagrad','Adam']
+    param_grid = dict(optimizer=optimizer)
+    
+    NNmodel = GridSearchCV(estimator=validator, param_grid=param_grid, scoring='neg_mean_squared_error', n_jobs=-1, cv=None)
+    NNmodel.fit(xtrain, ytrain, batch_size=None)
+    
+    
+
+
+    
+    
+
+    
+
+    
+    
+        
+
+    
+    
+
+    
+        
---
+++ xlsx_utils.py
@@ -0,0 +1,148 @@
+"""
+Microsoft Excel XLSX file utilities.
+
+@author: Nik Mohamad Aizuddin bin Nik Azmi
+@email: nik-mohamad-aizuddin@yandex.com
+@version: 0.1.0
+@date: 23/May/2018
+"""
+
+import csv
+import numpy as np
+from openpyxl import load_workbook
+
+def read(fname_xlsx):
+    """Load features and ideal output from XLSX file.
+
+    Parameters
+    ----------
+    fname_xlsx : str
+        Excel XLSX filename to read.
+
+    Returns
+    -------
+    features : np.ndarray
+        Features with shape (n,3).
+    ideal_output : np.ndarray
+        Ideal output with shape (n,2).
+    header : list
+        XLSX table header.
+    """
+
+    # Load Sheet1 from excel file.
+    workbook = load_workbook(filename=fname_xlsx)
+    sheet = workbook['Sheet1']
+
+    # Extract table header.
+    header = []
+    columns = sheet.max_column
+    for i in sheet.iter_cols(min_col=1, max_col=columns):
+        header.append(i[0].value)
+
+    # Extract number of room, room size, and corridor as features.
+    # Also extract the value limit and colony as the ideal output.
+    features = []
+    ideal_output = []
+    
+    #change here for different dataset read 
+    rows = sheet.max_row   
+    for i in sheet.iter_rows(min_row=2, max_row=rows):   
+        features.append([i[k].value for k in range(0, (columns-2))])
+        ideal_output.append([i[k].value for k in range((columns-2), columns)])
+
+    # Convert from list to numpy array.
+    features = np.asarray(features).astype(np.float)
+    ideal_output = np.asarray(ideal_output).astype(np.float)
+
+    return features, ideal_output, header
+
+
+def read_train(fname_xlsx):
+    """Load features and ideal output from XLSX file.
+
+    Parameters
+    ----------
+    fname_xlsx : str
+        Excel XLSX filename to read.
+
+    Returns
+    -------
+    features : np.ndarray
+        Features with shape (n,3).
+    ideal_output : np.ndarray
+        Ideal output with shape (n,2).
+    header : list
+        XLSX table header.
+    """
+
+    # Load Sheet1 from excel file.
+    workbook = load_workbook(filename=fname_xlsx)
+    sheet = workbook['Sheet1']
+
+    # Extract table header.
+    header = []
+    columns = sheet.max_column  
+    for i in sheet.iter_cols(min_col=1, max_col=columns):
+        header.append(i[0].value)
+
+    # Extract number of room, room size, and corridor as features.
+    # Also extract the value limit and colony as the ideal output.
+    features = []
+    ideal_output = []
+    
+    #change here for different dataset read 
+    rows = sheet.max_row
+    for i in sheet.iter_rows(min_row=2, max_row=rows):   
+        features.append([i[k].value for k in range(0, (columns-2))])
+        ideal_output.append([i[k].value for k in range((columns-2), columns)])
+
+    # Convert from list to numpy array.
+    features = np.asarray(features).astype(np.float)
+    ideal_output = np.asarray(ideal_output).astype(np.float)
+
+    return features, ideal_output, header
+
+def read_test(fname_xlsx):
+    """Load features and ideal output from XLSX file.
+
+    Parameters
+    ----------
+    fname_xlsx : str
+        Excel XLSX filename to read.
+
+    Returns
+    -------
+    features : np.ndarray
+        Features with shape (n,3).
+    ideal_output : np.ndarray
+        Ideal output with shape (n,2).
+    header : list
+        XLSX table header.
+    """
+
+    # Load Sheet1 from excel file.
+    workbook = load_workbook(filename=fname_xlsx)
+    sheet = workbook['Sheet1']
+
+    # Extract table header.
+    header = []
+    columns = sheet.max_column 
+    for i in sheet.iter_cols(min_col=1, max_col=columns):
+        header.append(i[0].value)
+
+    # Extract number of room, room size, and corridor as features.
+    # Also extract the value limit and colony as the ideal output.
+    features = []
+    ideal_output = []
+    
+    #change here for different dataset read 
+    rows = sheet.max_row
+    for i in sheet.iter_rows(min_row=2, max_row=rows):   
+        features.append([i[k].value for k in range(0, (columns-2))])
+        ideal_output.append([i[k].value for k in range((columns-2), columns)])
+
+    # Convert from list to numpy array.
+    features = np.asarray(features).astype(np.float)
+    ideal_output = np.asarray(ideal_output).astype(np.float)
+
+    return features, ideal_output, header
